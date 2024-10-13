a169e288eeb8166ff9f1a44116ff6215bc75da39
cyokozai <yuu4suke27@outlook.jp>
2024-10-13 15:11:35 +0000

    Refactor select_random_elite function to improve code readability and organization

--- src/cvt.jl
+++ src/cvt.jl
@@ -16,3 +16,13 @@ include("logger.jl")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+function select_random_elite(population::Population, area::Dict{Int64, Individual})
+    while true
+        i = rand(RNG, 1:GRID_SIZE)
+        j = rand(RNG, 1:GRID_SIZE)
+        
+        if grid[i, j] != 1 return population.individuals[grid[i, j]] end
+    end
+end
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
