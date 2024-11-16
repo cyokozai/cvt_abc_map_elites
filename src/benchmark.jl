cc3417b933d8fa6b0d5654e920572546d2266db7
cyokozai <yuu4suke27@outlook.jp>
2024-11-16 19:30:11 +0000

    DEアルゴリズムのパラメータ設定を条件分岐で整理し、各目的関数に対する交差率とスケーリングファクターを明確化。無効な目的パラメータに対するエラーハンドリングを追加。

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -33,65 +33,30 @@ if OBJ_F == "sphere"
     SOLUTION = zeros(D) # Number of solution
     UPP =  100.0        # Upper bound
     LOW = -100.0        # Lower bound
-
-    if METHOD == "DE"
-        CR  = 0.01          # Crossover rate
-        F   = 0.50          # Scaling factor
-    end
 elseif OBJ_F == "rosenbrock"
     SOLUTION = ones(D) # Number of solution
     UPP =  30.0        # Upper bound
     LOW = -30.0        # Lower bound
-
-    if METHOD == "DE"
-        CR  = 0.75          # Crossover rate
-        F   = 0.70          # Scaling factor
-    end
 elseif OBJ_F == "rastrigin"
     SOLUTION = zeros(D) # Number of solution
     UPP =  5.12         # Upper bound
     LOW = -5.12         # Lower bound
-
-    if METHOD == "DE"
-        CR  = 0.001         # Crossover rate
-        F   = 0.50          # Scaling factor
-    end
 elseif OBJ_F == "griewank"
     SOLUTION = zeros(D) # Number of solution
     UPP =  600.0        # Upper bound
     LOW = -600.0        # Lower bound
-    
-    if METHOD == "DE"
-        CR  = 0.20          # Crossover rate
-        F   = 0.50          # Scaling factor
-    end
 elseif OBJ_F == "ackley"
     SOLUTION = zeros(D) # Number of solution
     UPP =  32.0         # Upper bound
     LOW = -32.0         # Lower bound
-
-    if METHOD == "DE"
-        CR  = 0.20          # Crossover rate
-        F   = 0.50          # Scaling factor
-    end
 elseif OBJ_F == "schwefel"
     SOLUTION = zeros(D) # Number of solution
     UPP =  500.0        # Upper bound
     LOW = -500.0        # Lower bound
-
-    if METHOD == "DE"
-        CR  = 0.20          # Crossover rate
-        F   = 0.50          # Scaling factor
-    end
 elseif OBJ_F == "michalewicz"
     SOLUTION = zeros(D) # Number of solution
     UPP = 3.0           # Upper bound
     LOW = 0.0           # Lower bound
-
-    if METHOD == "DE"
-        CR  = 0.20          # Crossover rate
-        F   = 0.50          # Scaling factor
-    end
 else
     logger("ERROR", "Objective parameter is invalid")
 
