fabed8757a0eb3990d78934ced9642eaf448238a
cyokozai <yuu4suke27@outlook.jp>
2024-12-21 20:01:04 +0000

    docker-compose.yamlとmake-vorn.jlの修正により、結果のディレクトリ構造を変更し、ファイル名に日付を追加しました。また、出力先を整理しました。

--- make-vorn.jl
+++ make-vorn.jl
@@ -27,12 +27,12 @@ load_path = if ARGS[1] == "test"
 
     [path for path in readdir(dir) if occursin("test-", path) && occursin("CVT-", path)]
 else
-    dir = "./result/$(ARGS[2])/$(ARGS[4])/"
+    dir = "./result/$(ARGS[3])/$(ARGS[2])/"
     if !isdir(dir)
         error("Directory $dir does not exist.")
     end
 
-    [path for path in readdir(dir) if occursin("CVT-", path) && occursin("$(ARGS[5])", path) && occursin("-$(ARGS[3])-", path) && occursin("-$(ARGS[4])-", path)]
+    [path for path in readdir(dir) if occursin("CVT-", path) && occursin("$(ARGS[3])-cvt-$(ARGS[2])-$(ARGS[1])-", path)]
 end
 
 if isempty(load_path)
@@ -41,6 +41,10 @@ end
 
 loadpath = joinpath(dir, load_path[end])
 println(loadpath)
+
+m = match(r"CVT-(\d{4}-\d{2}-\d{2}-\d{2}-\d{2})", load_path[end])
+filedate = m !== nothing ? m.captures[1] : ""
+println("Extracted date: ", filedate)
 
 load_vorn = load(loadpath, "voronoi")
 Centroidal_polygon_list = DelaunayTriangulation.get_generators(load_vorn)
@@ -53,12 +57,12 @@ filepath = if ARGS[1] == "test"
     
     [path for path in readdir(dir) if occursin("test-", path) && occursin("result-", path)]
 else
-    dir = "./result/$(ARGS[2])/$(ARGS[4])/"
+    dir = "./result/$(ARGS[3])/$(ARGS[2])/"
     if !isdir(dir)
         error("Directory $dir does not exist.")
     end
 
-    [path for path in readdir(dir) if occursin("result-", path) && occursin("$(ARGS[5])", path)]
+    [path for path in readdir(dir) if occursin("result-$(filedate)-$(ARGS[3])-cvt-$(ARGS[2])-$(ARGS[1]).", path)]
 end
 
 Data = Vector{Int64}()  # Change the type to Vector{Int64}
@@ -175,12 +179,12 @@ filepath = if ARGS[1] == "test"
     
     [path for path in readdir(dir) if occursin("test-", path) && occursin("behavior-", path)]
 else
-    dir = "./result/$(ARGS[2])/$(ARGS[4])/"
+    dir = "./result/$(ARGS[3])/$(ARGS[2])/"
     if !isdir(dir)
         error("Directory $dir does not exist.")
     end
 
-    [path for path in readdir(dir) if occursin("behavior-", path) && occursin("$(ARGS[5])", path)]
+    [path for path in readdir(dir) if occursin("behavior-$(filedate)-$(ARGS[3])-cvt-$(ARGS[2])-$(ARGS[1]).", path)]
 end
 
 Data = Vector{Tuple{Float64, Float64}}()  # Change the type to Vector{Tuple{Float64, Float64}}
@@ -231,12 +235,14 @@ poly!(
 
 resize_to_layout!(fig)
 
+mkdir("result/cvt")
+
 if ARGS[1] == "test"
     println("Saved: result/testdata/pdf/testdata.pdf")
     save("result/testdata/pdf/behavior-testdata.pdf", fig)
 else
-    println("Saved: result/$(ARGS[2])/$(ARGS[4])/pdf/$(ARGS[2])-$(ARGS[4])-$(ARGS[1])-$(ARGS[5]).pdf")
-    save("result/$(ARGS[2])/$(ARGS[4])/pdf/$(ARGS[2])-$(ARGS[4])-$(ARGS[1])-$(ARGS[5]).pdf", fig)
+    println("Saved: result/cvt/$(ARGS[3])-$(ARGS[2])-$(ARGS[1]).pdf")
+    save("result/cvt/$(ARGS[3])-$(ARGS[2])-$(ARGS[1]).pdf", fig)
 end
 
 # # メイン処理
